0b0e3558458593f4487a21a51940114212ce9ae8
Hareesh123NH <162862005+Hareesh123NH@users.noreply.github.com>
2024-09-22 15:53:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
 //your JS code here. If required.
+const div=document.querySelector(".container");
+for (let i = 0; i < 800; i++) {
+	const square=document.createElement("span");
+	square.classList.add("square");
+	div.appendChild(square);
+}
+const squares=document.querySelectorAll(".square");
+squares.forEach((square)=>{
+	square.addEventListener("mouseover",()=>{
+		changeColor(square);
+
+        setTimeout(() => {
+            removeColor(square);
+        }, 1000);
+	});
+});
+
+function changeColor(square) {
+    const randomColor = getRandomColor();
+    square.style.backgroundColor = randomColor;
+}
+
+function removeColor(square) {
+    square.style.backgroundColor = '#1b1b1b';
+}
+
+function getRandomColor() {
+    const hexColors = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += hexColors[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
